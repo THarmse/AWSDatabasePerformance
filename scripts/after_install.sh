42370b04c11983d5b274352bb6c955e805ac1b91
Theodor Harmse <harmsetheo@gmail.com>
2025-06-29 22:21:04 +0400

    appspec fix

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,16 +3,13 @@ set -ex
 echo "Running AfterInstall script"
 
 cd /home/ec2-user/app
-
-echo "Listing contents for debugging:"
-ls -la
-
-if [ ! -f requirements.txt ]; then
-  echo "ERROR: requirements.txt is missing or not a file!"
-  exit 1
-fi
-
 python3 -m venv venv
 source venv/bin/activate
+
+echo "Upgrading pip"
 pip install --upgrade pip
+
+echo "Installing requirements.txt"
 pip install -r requirements.txt
+
+echo "AfterInstall script complete"
