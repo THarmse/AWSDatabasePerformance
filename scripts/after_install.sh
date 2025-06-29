5c1696ced5d439e252cb13fbe9d0c22b8255cf44
Theodor Harmse <harmsetheo@gmail.com>
2025-06-29 22:17:02 +0400

    deploy code

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,15 @@ set -ex
 echo "Running AfterInstall script"
 
 cd /home/ec2-user/app
+
+echo "Listing contents for debugging:"
+ls -la
+
+if [ ! -f requirements.txt ]; then
+  echo "ERROR: requirements.txt is missing or not a file!"
+  exit 1
+fi
+
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
