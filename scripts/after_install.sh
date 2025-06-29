5e74f7d8d3a202a7adbd3c5491e7b81c51254e9e
Theodor Harmse <harmsetheo@gmail.com>
2025-06-29 23:01:41 +0400

    after-Install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,22 +3,34 @@ set -ex
 echo "Running AfterInstall script"
 
 cd /home/ec2-user/app
+
+# Create and activate venv
 python3 -m venv venv
 source venv/bin/activate
+
+# Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
-cat >/etc/httpd/conf.d/app_proxy.conf <<'EOF'
+# Add Apache proxy configuration
+cat <<EOF > /etc/httpd/conf.d/app.conf
 <VirtualHost *:80>
     DocumentRoot /var/www/html
+
     ProxyPreserveHost On
+    ProxyRequests Off
 
-    ProxyPass "/mysql/" "http://127.0.0.1:8000/mysql/"
-    ProxyPassReverse "/mysql/" "http://127.0.0.1:8000/mysql/"
+    <Proxy *>
+        Require all granted
+    </Proxy>
 
-    ProxyPass "/docs" "http://127.0.0.1:8000/docs"
-    ProxyPassReverse "/docs" "http://127.0.0.1:8000/docs"
+    ProxyPass /mysql/ http://127.0.0.1:8000/mysql/
+    ProxyPassReverse /mysql/ http://127.0.0.1:8000/mysql/
+
+    ProxyPass /docs http://127.0.0.1:8000/docs
+    ProxyPassReverse /docs http://127.0.0.1:8000/docs
 </VirtualHost>
 EOF
 
+# Restart Apache
 systemctl restart httpd
