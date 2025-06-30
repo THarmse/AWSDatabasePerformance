b0af09c5edbabd98692a77f168159505e29ee0d1
Theodor Harmse <harmsetheo@gmail.com>
2025-07-01 01:44:06 +0400

    sleep

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -23,25 +23,7 @@ sudo yum makecache
 # Install Microsoft ODBC Driver 17 for SQL Server
 sudo ACCEPT_EULA=Y yum install -y msodbcsql17
 
-# Wait for files to appear (Oracle)
-echo "Waiting for Oracle Instant Client RPMs to appear..."
-for i in {1..60}; do
-  if ls /home/ec2-user/app/oracle_rpms/*.rpm >/dev/null 2>&1; then
-    echo "RPMs detected!"
-    break
-  fi
-  echo "Still waiting..."
-  sleep 1
-done
-
-# Set wide open permissions
-echo "Setting permissions..."
-sudo chmod 777 /home/ec2-user/app/oracle_rpms
-sudo chmod 777 /home/ec2-user/app/oracle_rpms/*.rpm
-
-# Log contents
-echo "RPM directory listing:"
-ls -lh /home/ec2-user/app/oracle_rpms
+sleep 120
 
 # Install Oracle Instant Client RPMs
 echo "Installing Oracle Instant Client RPMs..."
