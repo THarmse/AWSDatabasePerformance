221cf580480f6f631e4e8fa15e33cdba8d0436a6
Theodor Harmse <harmsetheo@gmail.com>
2025-07-06 21:22:13 +0400

    Connection pooling

--- api_service/db/base.py
+++ api_service/db/base.py
@@ -5,37 +5,47 @@
 import json
 import os
 import boto3
-import pymysql
+import pyodbc
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from threading import Lock
+
 import psycopg2
 import psycopg2.extras
-import pyodbc
 
 REGION = os.environ.get("AWS_REGION", "eu-west-1")
 
+# Caching for parameter store
+from parameter_store import get_db_credentials
 
-def get_db_credentials(param_name: str) -> str:
-    """
-    Fetch credentials from AWS Systems Manager Parameter Store.
-    """
-    ssm = boto3.client('ssm', region_name=REGION)
-    response = ssm.get_parameter(Name=param_name, WithDecryption=True)
-    return response['Parameter']['Value']
+# Connection pool engines
+_mysql_engine = None
+_postgresql_engine = None
+_mariadb_engine = None
+_mssql_engine = None
+_oracle_engine = None
+_ibmdb2_engine = None
 
+_lock = Lock()
+
+def _get_mysql_engine(param_name: str) -> Engine:
+    global _mysql_engine
+    with _lock:
+        if _mysql_engine is None:
+            creds = json.loads(get_db_credentials(param_name))
+            _mysql_engine = create_engine(
+                f"mysql+pymysql://{creds['username']}:{creds['password']}@{creds['host']}:{creds['port']}/{creds['database']}",
+                pool_size=200,
+                max_overflow=100,
+                pool_recycle=3600
+            )
+        return _mysql_engine
 
 def get_mysql_connection(param_name: str):
     """
-    MySQL connection using PyMySQL.
+    MySQL connection using PyMySQL via SQLAlchemy Engine Pool.
     """
-    creds = json.loads(get_db_credentials(param_name))
-    return pymysql.connect(
-        host=creds['host'],
-        user=creds['username'],
-        password=creds['password'],
-        database=creds['database'],
-        port=int(creds.get('port', 3306)),
-        cursorclass=pymysql.cursors.DictCursor
-    )
-
+    return _get_mysql_engine(param_name).connect()
 
 def get_aurora_mysql_connection(param_name: str):
     """
@@ -43,21 +53,24 @@ def get_aurora_mysql_connection(param_name: str):
     """
     return get_mysql_connection(param_name)
 
+def _get_postgresql_engine(param_name: str) -> Engine:
+    global _postgresql_engine
+    with _lock:
+        if _postgresql_engine is None:
+            creds = json.loads(get_db_credentials(param_name))
+            _postgresql_engine = create_engine(
+                f"postgresql+psycopg2://{creds['username']}:{creds['password']}@{creds['host']}:{creds['port']}/{creds['database']}",
+                pool_size=200,
+                max_overflow=100,
+                pool_recycle=3600
+            )
+        return _postgresql_engine
 
 def get_postgresql_connection(param_name: str):
     """
-    PostgreSQL connection using psycopg2.
+    PostgreSQL connection using psycopg2 via SQLAlchemy Engine Pool.
     """
-    creds = json.loads(get_db_credentials(param_name))
-    return psycopg2.connect(
-        host=creds['host'],
-        user=creds['username'],
-        password=creds['password'],
-        dbname=creds['database'],
-        port=int(creds.get('port', 5432)),
-        cursor_factory=psycopg2.extras.RealDictCursor
-    )
-
+    return _get_postgresql_engine(param_name).connect()
 
 def get_aurora_postgresql_connection(param_name: str):
     """
@@ -65,62 +78,81 @@ def get_aurora_postgresql_connection(param_name: str):
     """
     return get_postgresql_connection(param_name)
 
-
-def get_mssqlserver_connection(param_name: str):
-    """
-    Microsoft SQL Server connection using pyodbc.
-    """
-    creds = json.loads(get_db_credentials(param_name))
-    conn_str = (
-        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
-        f"SERVER={creds['host']},{creds.get('port', 1433)};"
-        f"DATABASE={creds['database']};"
-        f"UID={creds['username']};"
-        f"PWD={creds['password']}"
-    )
-    return pyodbc.connect(conn_str)
-
-
-def get_oracle_connection(param_name: str):
-    """
-    Oracle 19c connection using cx_Oracle.
-    The service_name is always 'ORCL'.
-    """
-    import cx_Oracle
-    creds = json.loads(get_db_credentials(param_name))
-    dsn = cx_Oracle.makedsn(
-        creds['host'],
-        int(creds.get('port', 1521)),
-        service_name=creds['database']
-    )
-    return cx_Oracle.connect(
-        user=creds['username'],
-        password=creds['password'],
-        dsn=dsn
-    )
+def _get_mariadb_engine(param_name: str) -> Engine:
+    global _mariadb_engine
+    with _lock:
+        if _mariadb_engine is None:
+            creds = json.loads(get_db_credentials(param_name))
+            _mariadb_engine = create_engine(
+                f"mysql+pymysql://{creds['username']}:{creds['password']}@{creds['host']}:{creds['port']}/{creds['database']}",
+                pool_size=200,
+                max_overflow=100,
+                pool_recycle=3600
+            )
+        return _mariadb_engine
 
 def get_mariadb_connection(param_name: str):
     """
-    MariaDB connection using PyMySQL.
+    MariaDB connection using PyMySQL via SQLAlchemy Engine Pool.
     """
-    return get_mysql_connection(param_name)
+    return _get_mariadb_engine(param_name).connect()
+
+def _get_mssql_engine(param_name: str) -> Engine:
+    global _mssql_engine
+    with _lock:
+        if _mssql_engine is None:
+            creds = json.loads(get_db_credentials(param_name))
+            _mssql_engine = create_engine(
+                f"mssql+pyodbc://{creds['username']}:{creds['password']}@{creds['host']}:{creds.get('port', 1433)}/{creds['database']}?driver=ODBC+Driver+17+for+SQL+Server",
+                pool_size=200,
+                max_overflow=100,
+                pool_recycle=3600
+            )
+        return _mssql_engine
+
+def get_mssqlserver_connection(param_name: str):
+    """
+    Microsoft SQL Server connection using SQLAlchemy Engine Pool.
+    """
+    return _get_mssql_engine(param_name).connect()
+
+def _get_oracle_engine(param_name: str) -> Engine:
+    global _oracle_engine
+    with _lock:
+        if _oracle_engine is None:
+            creds = json.loads(get_db_credentials(param_name))
+            _oracle_engine = create_engine(
+                f"oracle+cx_oracle://{creds['username']}:{creds['password']}@{creds['host']}:{creds.get('port', 1521)}/?service_name={creds['database']}",
+                pool_size=200,
+                max_overflow=100,
+                pool_recycle=3600
+            )
+        return _oracle_engine
+
+def get_oracle_connection(param_name: str):
+    """
+    Oracle 19c connection using cx_Oracle via SQLAlchemy Engine Pool.
+    """
+    return _get_oracle_engine(param_name).connect()
+
+def _get_ibmdb2_engine(param_name: str) -> Engine:
+    global _ibmdb2_engine
+    with _lock:
+        if _ibmdb2_engine is None:
+            creds = json.loads(get_db_credentials(param_name))
+            _ibmdb2_engine = create_engine(
+                f"ibm_db_sa://{creds['username']}:{creds['password']}@{creds['host']}:{creds.get('port', 50000)}/{creds['database']}",
+                pool_size=200,
+                max_overflow=100,
+                pool_recycle=3600
+            )
+        return _ibmdb2_engine
 
 def get_ibm_db2_connection(param_name: str):
     """
-    IBM DB2 connection using ibm_db_dbi.
+    IBM DB2 connection using ibm_db_sa via SQLAlchemy Engine Pool.
     """
-    import ibm_db_dbi
-    creds = json.loads(get_db_credentials(param_name))
-    conn_str = (
-        f"DATABASE={creds['database']};"
-        f"HOSTNAME={creds['host']};"
-        f"PORT={int(creds.get('port', 50000))};"
-        f"PROTOCOL=TCPIP;"
-        f"UID={creds['username']};"
-        f"PWD={creds['password']};"
-    )
-    return ibm_db_dbi.connect(conn_str, "", "")
-
+    return _get_ibmdb2_engine(param_name).connect()
 
 def get_dynamodb_resource():
     """
