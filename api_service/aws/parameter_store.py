464bb7fcbe2c00c74a207128c235b539676e1c95
Theodor Harmse <harmsetheo@gmail.com>
2025-07-06 20:27:16 +0400

    TTl for paramter store

--- api_service/aws/parameter_store.py
+++ api_service/aws/parameter_store.py
@@ -4,19 +4,29 @@
 
 import boto3
 import os
-from functools import lru_cache
+from cachetools import TTLCache
+from threading import Lock
 
 REGION = os.environ.get("AWS_REGION", "eu-west-1")
 
-@lru_cache(maxsize=128)
+_cache = TTLCache(maxsize=128, ttl=3600)  # 1 hour TTL
+_lock = Lock()
+
 def get_db_credentials(param_name: str) -> str:
     """
-    Returns the cached value if present.
-    Otherwise, fetches from Parameter Store and caches it in memory per process.
+    Returns the cached value if present and not expired.
+    Otherwise, fetches from Parameter Store, caches it in memory.
+    Thread-safe.
     """
-    ssm_client = boto3.client('ssm', region_name=REGION)
-    response = ssm_client.get_parameter(
-        Name=param_name,
-        WithDecryption=True
-    )
-    return response['Parameter']['Value']
+    with _lock:
+        if param_name in _cache:
+            return _cache[param_name]
+
+        ssm_client = boto3.client('ssm', region_name=REGION)
+        response = ssm_client.get_parameter(
+            Name=param_name,
+            WithDecryption=True
+        )
+        value = response['Parameter']['Value']
+        _cache[param_name] = value
+        return value
